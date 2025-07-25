b567e36d3145b2092481398c46b42e1f25df8f76
Ketan Sharma <ketansharma.ks.911@gmail.com>
2025-07-25 15:23:02 +0530

    commit2

--- src/server.js
+++ src/server.js
@@ -4,15 +4,23 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js"
 import { initDB } from "./config/db.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+if (process.env.NODE_ENV === "production") job.start();
+
 // middleware
 app.use(rateLimiter);
 app.use(express.json());
+
+// check api is working or not
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "Ok" });
+})
 
 // custom middleware
 app.use((req, res, next) => {
